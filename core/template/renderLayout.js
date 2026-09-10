b1ed61edfa2c10a648f30dd628716eef05ed6bcb
Yuo_O <1635276708@qq.com>
2026-09-10 12:30:34 +0800

    chore(renderLayout): add verify google seach `meta`

--- core/template/renderLayout.js
+++ core/template/renderLayout.js
@@ -62,6 +62,7 @@ export function renderLayout({
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+  <meta name="google-site-verification" content="uksFuN6QnFtkgoxixLocaSjvWP417ogPMpEcfoEDcgw">
 ${meta}
   <link rel="stylesheet" href="${bp}/assets/reset.css">
   <link rel="stylesheet" href="${bp}/assets/tokens.css">
